40b081016a9b6269fdd8a35cd74668eed698578e
Bruce Munoz <107725994+Bmuno23@users.noreply.github.com>
2024-10-05 15:40:25 -0400

    added finishing styles

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-//create 16x16 grid of div's
 
 const container = document.querySelector("#container");
 
 function createGrid(size){
+    //clear existing grid
     container.innerHTML = '';
+
+    //create grid based on size input
     for (let i = 0; i < size * size; i++) {
         const div = document.createElement("div");
     
@@ -32,10 +34,16 @@ function createGrid(size){
 //create initial grid
 createGrid(16);
 
+//create new sized grid based on user input if button is clicked
 const size = document.querySelector("#changeSize")
 size.addEventListener("click", () =>{
+
     let val = prompt("Please Enter Desired Size");
-    createGrid(val);
+    if (!isNaN(val) && val > 0) {
+        createGrid(val); // Create a new grid with the specified size
+      } else {
+        alert("Please enter a valid number!");
+      }
 })
 
 
